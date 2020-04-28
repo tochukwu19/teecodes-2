c6b4576b67fb1ccdbfbfb1bcc18ae9589d7d3598
Tochukwu Onwunali <onwunali91@yahoo.com>
2020-04-28 19:04:13 +0000

    Done with the about

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,6 @@ import Work1 from "../images/work1.png"
 import Work2 from "../images/work2.png"
 import Work3 from "../images/logo.svg"
 import Work3D from "../images/logodark.svg"
-import right from "../images/right.svg"
-import rightD from "../images/rightdark.svg"
 import {Link} from "gatsby"
 
 
@@ -113,7 +111,16 @@ const IndexPage = () => {
         <em>Need a developer?</em>
         <div>
             <Link to="/contact"><h2>Let's work together</h2></Link>
-            { !isDark ? <img src={right} alt="arrow" className="arrow"/> : <img src={rightD} alt="arrow" className="arrow"/>}
+              <svg id="Layer_1" viewBox="0 0 512 512">
+                <g>
+                  <g>
+                    <path d="M508.875,248.458l-160-160c-4.167-4.167-10.917-4.167-15.083,0c-4.167,4.167-4.167,10.917,0,15.083l141.792,141.792
+                      H10.667C4.771,245.333,0,250.104,0,256s4.771,10.667,10.667,10.667h464.917L333.792,408.458c-4.167,4.167-4.167,10.917,0,15.083
+                      c2.083,2.083,4.813,3.125,7.542,3.125c2.729,0,5.458-1.042,7.542-3.125l160-160C513.042,259.375,513.042,252.625,508.875,248.458z
+                      "/>
+                  </g>
+                </g>
+              </svg>
         </div>
       </section>
       <section className="footnav">
