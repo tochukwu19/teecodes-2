ef07651ae763c6456b72c1a28d498ed72fde3b71
Tochukwu Onwunali <onwunali91@yahoo.com>
2020-05-21 16:55:34 +0000

    Yo

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,8 +46,8 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#0091AD`,
+        theme_color: `#0091AD`,
         display: `minimal-ui`,
         icon: `src/images/text.png`, // This path is relative to the root of the site.
       },
